9a8c78db02fb5cd1d48a104f07eba7ac19727096
Òscar Casajuana <elboletaire@underave.net>
2019-05-23 23:50:10 +0200

    Now merges audios and videos (no subs for now)

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"remuxing/info"
 	"strings"
+
+	"github.com/logrusorgru/aurora"
 )
 
 func merge() {
@@ -22,32 +23,6 @@ func merge() {
 func syntaxError(err string) {
 	fmt.Println(fmt.Sprintf("syntax error: %s", err))
 	os.Exit(1)
-}
-
-func getFileInfo(file string) (info info.Info) {
-	fmt.Println("Getting file info for file:", file)
-
-	output, mergeErr := exec.Command(
-		"mkvmerge",
-		"-F",
-		"json",
-		"-i",
-		file,
-	).CombinedOutput()
-
-	if mergeErr != nil {
-		fmt.Println(fmt.Sprint(mergeErr) + ": " + string(output))
-		return
-	}
-
-	if err := json.Unmarshal(output, &info); err != nil {
-		panic(err)
-	}
-
-	// We want it in minutes, so we don't care about decimals
-	info.Container.Properties.Duration = info.Container.Properties.Duration / 1000 / 1000 / 1000
-
-	return
 }
 
 func parseArgs() (output string, inputs []string, languages []string) {
@@ -76,30 +51,136 @@ func parseArgs() (output string, inputs []string, languages []string) {
 	return
 }
 
-func filterInfos(infos []info.Info, test func(info.Info) bool) (ret []info.Info) {
-	for _, information := range infos {
-		if test(information) {
-			ret = append(ret, information)
-		}
+func argIDLabel(id uint, lab string) string {
+	result := []string{
+		fmt.Sprint(id),
+		":",
+		lab,
 	}
 
-	return
+	return strings.Join(result, "")
 }
 
-func filterSupported(info info.Info) bool {
-	return info.Container.Supported
+func printBestTrack(track info.Track) {
+	fmt.Printf(
+		aurora.Green("- Track ID %d (%s) from file %s\n").String(),
+		track.ID,
+		track.Properties.Language,
+		track.Parent.FileName,
+	)
+}
+
+func title(text string) {
+	fmt.Printf(
+		aurora.Yellow("\n\n  %s\n  %s %s %s\n  %s\n\n").String(),
+		strings.Repeat("#", len(text)+4),
+		"#",
+		text,
+		"#",
+		strings.Repeat("#", len(text)+4),
+	)
 }
 
 func main() {
-	_, inputs, _ := parseArgs()
+	output, inputs, languages := parseArgs()
 
 	var infos []info.Info
-	for _, input := range inputs {
-		infos = append(infos, getFileInfo(input))
-	}
-	supported := filterInfos(infos, filterSupported)
+	var videos []info.Track
+	var audios []info.Track
+	var subtitles []info.Track
 
-	fmt.Printf("%+v\n", supported)
+	title("INPUTS")
+	for pos, input := range inputs {
+		fmt.Printf(
+			aurora.Green("%d. Getting file info for file: %s\n").String(),
+			pos+1,
+			input,
+		)
+		information := info.GetFileInfo(input)
+		information.SetParents()
+		information.SetPosition(pos)
+
+		if !information.Container.Supported {
+			fmt.Println(aurora.Sprintf(aurora.Red("File container is not supported for file %s"), input))
+			continue
+		}
+
+		for _, track := range information.Tracks {
+			switch track.Type {
+			case "video":
+				videos = append(videos, track)
+			case "audio":
+				audios = append(audios, track)
+			case "subtitles":
+				subtitles = append(subtitles, track)
+			}
+		}
+
+		infos = append(infos, information)
+	}
+
+	title("VIDEO")
+	bestVideo := info.DecideBestVideo(videos)
+
+	printBestTrack(bestVideo)
+
+	title("AUDIO")
+	bestAudios := info.DecideBestAudios(audios, languages)
+	for _, audio := range bestAudios {
+		printBestTrack(audio)
+	}
+
+	args := []string{
+		"-o", output,
+		// Empty name (just in case, should be configurable tho)
+		"--title", "",
+		// Do not copy audio from the video source
+		"-A",
+		// Do not copy tracks info from this file
+		"-T",
+		// Do not copy subtitles either
+		"-S",
+		// Specify video id to be copied
+		"-d", fmt.Sprint(bestVideo.ID),
+		// Video route
+		bestVideo.Parent.FileName,
+	}
+
+	for i, audio := range bestAudios {
+		if i == 0 {
+			args = append(args, "--default-track", fmt.Sprint(audio.ID))
+		}
+
+		// Ensure audio stream has language set
+		args = append(
+			args,
+			"--language", argIDLabel(audio.ID, audio.Properties.Language),
+		)
+		// Copy this audio stream
+		args = append(args, "-a", fmt.Sprint(audio.ID))
+		// Remove its file name
+		args = append(args, "--track-name", argIDLabel(audio.ID, ""))
+		// Do not copy videos from this file
+		args = append(args, "-D")
+		// Do not copy subtitles from this file
+		args = append(args, "-S")
+		// Set the filepath
+		args = append(args, audio.Parent.FileName)
+	}
+
+	title("COMMAND")
+	fmt.Printf(aurora.Gray(15, "$ mkvmerge %s\n").String(), strings.Join(args, " "))
+
+	title("RESULT")
+
+	result, err := exec.Command("mkvmerge", args...).CombinedOutput()
+
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + string(result))
+		return
+	}
+
+	fmt.Println(string(result))
 
 	// fmt.Println(fmt.Sprintf("Output is %s", output))
 	// fmt.Println(fmt.Sprintf("Inputs are %s", strings.Join(inputs[:], ",")))
---
+++ readme.md
@@ -0,0 +1,9 @@
+Remuxing utility helper for mkvmerge
+====================================
+
+2does
+-----
+
+- [ ] Allow to use without languages setting, appending them all
+- [ ] Be able to specify the proper language id for a track (for cases where language is not properly set in the source)
+- [ ] Disable verbosity unless -verbose is defined

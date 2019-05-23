6634cde869cd0b795bfb683a6adf572cbd8c28c2
Òscar Casajuana <elboletaire@underave.net>
2019-05-24 00:56:39 +0200

    Coding style changes

--- main.go
+++ main.go
@@ -13,13 +13,63 @@ import (
 
 const gray = 13
 
-func merge() {
-	exec.Command(
-		"mkvmerge",
-		"--title",
-		"\"\"",
+func merge(
+	output string,
+	languages []string,
+	video info.Track, audios []info.Track, subtitles []info.Track,
+) string {
+	args := []string{
+		"-o", output,
+		// Empty name (just in case, should be configurable tho)
+		"--title", "",
+		// Do not copy audio from the video source
+		"-A",
+		// Do not copy tracks info from this file
 		"-T",
-	)
+		// Do not copy subtitles either
+		"-S",
+		// Specify video id to be copied
+		"-d", fmt.Sprint(video.ID),
+		// Video route
+		video.Parent.FileName,
+	}
+
+	for i, audio := range audios {
+		if i == 0 {
+			args = append(args, "--default-track", fmt.Sprint(audio.ID))
+		}
+
+		// Ensure audio stream has language set
+		args = append(
+			args,
+			"--language", argIDLabel(audio.ID, audio.Properties.Language),
+		)
+		// Copy this audio stream
+		args = append(args, "-a", fmt.Sprint(audio.ID))
+		// Remove its file name
+		args = append(args, "--track-name", argIDLabel(audio.ID, ""))
+		// Do not copy videos from this file
+		args = append(args, "-D")
+		// Do not copy subtitles from this file
+		args = append(args, "-S")
+		// Set the filepath
+		args = append(args, audio.Parent.FileName)
+	}
+
+	s := title("COMMAND")
+	s += fmt.Sprintf(aurora.Gray(15, "$ mkvmerge %s\n").String(), strings.Join(args, " "))
+
+	s += title("RESULT")
+
+	result, err := exec.Command("mkvmerge", args...).CombinedOutput()
+
+	if err != nil {
+		panic(fmt.Sprint(err) + ": " + string(result))
+	}
+
+	s += string(result)
+
+	return s
 }
 
 func syntaxError(err string) {
@@ -56,12 +106,13 @@ func parseArgs() (
 	inputs []string,
 	languages []string,
 	verbose bool,
-	help bool,
 ) {
 	flag.StringVar(&output, "output", "", "The output file.")
+
 	var lang string
 	flag.StringVar(&lang, "languages", "", "Languages to be taken from inputs. Order matters, first one will be marked as default track.")
 
+	var help bool
 	flag.BoolVar(&help, "h", false, "This help.")
 	flag.BoolVar(&verbose, "v", false, "Verbose.")
 	flag.Parse()
@@ -118,12 +169,8 @@ func printBestTrack(verbose bool, track info.Track) {
 	)
 }
 
-func title(verbose bool, text string) {
-	if !verbose {
-		return
-	}
-
-	fmt.Printf(
+func title(text string) string {
+	return fmt.Sprintf(
 		aurora.Yellow("\n\n  %s\n  %s %s %s\n  %s\n\n").String(),
 		strings.Repeat("#", len(text)+4),
 		"#",
@@ -133,15 +180,18 @@ func title(verbose bool, text string) {
 	)
 }
 
-func main() {
-	output, inputs, languages, verbose, _ := parseArgs()
+func extractTracks(inputs []string, languages []string, verbose bool) (
+	video info.Track,
+	audios []info.Track,
+	subtitles []info.Track,
+) {
 
-	var infos []info.Info
 	var videos []info.Track
-	var audios []info.Track
-	var subtitles []info.Track
 
-	title(verbose, "INPUTS")
+	if verbose {
+		fmt.Print(title("INPUTS"))
+	}
+
 	for pos, input := range inputs {
 		if verbose {
 			fmt.Printf(
@@ -171,83 +221,33 @@ func main() {
 				subtitles = append(subtitles, track)
 			}
 		}
-
-		infos = append(infos, information)
 	}
 
-	title(verbose, "VIDEO")
-	bestVideo := info.DecideBestVideo(videos)
+	if verbose {
+		fmt.Print(title("VIDEO"))
+	}
+	video = info.DecideBestVideo(videos)
+	printBestTrack(verbose, video)
 
-	printBestTrack(verbose, bestVideo)
-
-	title(verbose, "AUDIO")
-	bestAudios := info.DecideBestAudios(audios, languages)
-	for _, audio := range bestAudios {
+	if verbose {
+		fmt.Print(title("AUDIO"))
+	}
+	audios = info.DecideBestAudios(audios, languages)
+	for _, audio := range audios {
 		printBestTrack(verbose, audio)
 	}
 
-	args := []string{
-		"-o", output,
-		// Empty name (just in case, should be configurable tho)
-		"--title", "",
-		// Do not copy audio from the video source
-		"-A",
-		// Do not copy tracks info from this file
-		"-T",
-		// Do not copy subtitles either
-		"-S",
-		// Specify video id to be copied
-		"-d", fmt.Sprint(bestVideo.ID),
-		// Video route
-		bestVideo.Parent.FileName,
-	}
+	return
+}
 
-	for i, audio := range bestAudios {
-		if i == 0 {
-			args = append(args, "--default-track", fmt.Sprint(audio.ID))
-		}
+func main() {
+	output, inputs, languages, verbose := parseArgs()
 
-		// Ensure audio stream has language set
-		args = append(
-			args,
-			"--language", argIDLabel(audio.ID, audio.Properties.Language),
-		)
-		// Copy this audio stream
-		args = append(args, "-a", fmt.Sprint(audio.ID))
-		// Remove its file name
-		args = append(args, "--track-name", argIDLabel(audio.ID, ""))
-		// Do not copy videos from this file
-		args = append(args, "-D")
-		// Do not copy subtitles from this file
-		args = append(args, "-S")
-		// Set the filepath
-		args = append(args, audio.Parent.FileName)
-	}
+	video, audios, subtitles := extractTracks(inputs, languages, verbose)
 
-	title(verbose, "COMMAND")
-	if verbose {
-		fmt.Printf(aurora.Gray(15, "$ mkvmerge %s\n").String(), strings.Join(args, " "))
-	}
-
-	title(verbose, "RESULT")
-
-	result, err := exec.Command("mkvmerge", args...).CombinedOutput()
-
-	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + string(result))
-		return
-	}
+	result := merge(output, languages, video, audios, subtitles)
 
 	if verbose {
 		fmt.Println(string(result))
 	}
-
-	// fmt.Println(fmt.Sprintf("Output is %s", output))
-	// fmt.Println(fmt.Sprintf("Inputs are %s", strings.Join(inputs[:], ",")))
-	// fmt.Println(fmt.Sprintf("Languages are %s", strings.Join(languages[:], ",")))
-	// args := os.Args[1:]
-
-	// 1. Check expected params are received
-	//   - At least two inputs are received
-	//   - Output path flag (-o) is also mandatory
 }
